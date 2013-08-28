344f13770c1695404c85ee9af3ee9af294c1fd44
Joakim Runeberg <joakim.runeberg@gmail.com>
2013-08-28 15:54:06 +0300

    use new bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require twitter/bootstrap
 //= require bootstrap-datetimepicker
 //= require turbolinks
 //= require jquery.ui.all
